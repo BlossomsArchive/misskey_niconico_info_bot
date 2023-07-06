5be99db2004571ae6cff4142c3bcbbfb95633818
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-06 11:35:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
+import pathlib
+
 f = open("test.txt", "r")
 test_content = f.read()
 f.close()
 
 print(test_content)
 
-g = open("test.txt", "w")
-g.write("It has changed")
-g.close
+root = pathlib.Path(__file__).parent.resolve()
+text = root / "test.txt"
+with open(text,"w") as f:
+  f.write("It has changed")
 
 print("It has changed")
